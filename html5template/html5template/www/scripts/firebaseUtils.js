48eb6660dfabd29f2e40c79e24599da03aab5c9f
Chad <chad.cole.963@my.csun.edu>
2017-09-02 21:32:41 -0700

    In progress commit; firebase authentication
    Trying to update/add data to the database (user profile data).
    Trying to redirect login page to hexagon.html when a user logs in.

--- html5template/html5template/www/scripts/firebaseUtils.js
+++ html5template/html5template/www/scripts/firebaseUtils.js
@@ -1,29 +1,91 @@
     // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyBjF93oNeV9xMebcstkjtr-rV2WoRy6eh0",
-        authDomain: "letzchatenterprise.firebaseapp.com",
-        databaseURL: "https://letzchatenterprise.firebaseio.com",
-        projectId: "letzchatenterprise",
-        storageBucket: "",
-        messagingSenderId: "138258019105"
-    };
-    firebase.initializeApp(config);
-    var username = "";
-    var password = "";
-    var loginForm = document.getElementById('loginForm');
+var config = {
+    apiKey: "AIzaSyBjF93oNeV9xMebcstkjtr-rV2WoRy6eh0",
+    authDomain: "letzchatenterprise.firebaseapp.com",
+    databaseURL: "https://letzchatenterprise.firebaseio.com",
+    projectId: "letzchatenterprise",
+    storageBucket: "",
+    messagingSenderId: "138258019105"
+};
+firebase.initializeApp(config);
 
-    function onLoginSubmit() {
-        window.alert("onLoginSubmit hit");
-        username = document.getElementById('username').value;
-        password = document.getElementById('password').value;
+var fullName = "";
+var username = "";
+var password = "";
+var loginForm = document.getElementById('loginForm');
 
-        window.alert("username = " + username + "\npassword = " + password);
-        return false;
-    }
-    
-    firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
+function onLoginSubmit() {
+    username = document.getElementById('username').value;
+    password = document.getElementById('password').value;
+
+    console.log("username = " + username + "\npassword = " + password);
+
+    firebase.auth().signInWithEmailAndPassword(username, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode !== null) {
+            console.log(errorCode + ": " + errorMessage);
+        }
     });
+    //window.location.href = 'hexagon.html';
+
+    return false;
+}
+
+function onSignupSubmit() {
+
+    fullName = document.getElementById('fullName').value;
+    username = document.getElementById('username').value;
+    password = document.getElementById('password').value;
+
+    console.log("fullName = " + fullName + "\nusername = " + username + "\npassword = " + password);
+
+    firebase.auth().createUserWithEmailAndPassword(username, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode !== null) {
+            window.alert(errorCode + ": " + errorMessage);
+        }
+    });
+
+    console.log("Created user successfully");
+
+    var user = firebase.auth().currentUser;
+    if (user) {
+        user.sendEmailVerification();
+        console.log("Sent Email verification");
+        user.fullName = fullName;
+        writeUserData(user);
+    }
+
+    return false;
+}
+
+function writeUserData(user) {
+    printUser(user);
+    var userRef = firebase.database().ref("userId/" + user.uid);
+    console.log("userRef = " + userRef.toString());
+
+    var newData = userRef.push();
+    newData.set({
+        "email": user.email,
+        "fullName": user.fullName,
+        "location": user.location,
+        "password": user.password,
+        "photo_id": user.photo_id,
+        "prefix": user.prefix,
+        "title": user.title
+    });
+}
+
+function printUser(user) {
+    console.log("userId: " + user.uid +
+        "\nemail: " + user.email +
+        "\nfullName: " + user.fullName +
+        "\nlocation: " + user.location +
+        "\nphoto_id: " + user.photo_id +
+        "\nprefix: " + user.prefix +
+        "\ntitle: " + user.title);
+}
