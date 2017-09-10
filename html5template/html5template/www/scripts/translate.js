34cc7de010cd28059fdb9e3215595cdef32fd5f1
Chad <chad.cole.963@my.csun.edu>
2017-09-09 20:40:38 -0700

    Google Translate API call
    Created the function to call the Google Translate API and returns the result.

---
+++ html5template/html5template/www/scripts/translate.js
@@ -0,0 +1,28 @@
+﻿var apiKey = "AIzaSyCeJljhj9ChaPHoDuYr5O1RzljzjP9S040";
+
+function translate(txtSource, sourceLang) {
+    var deviceLanguage = localStorage.getItem("device_language");
+    if (deviceLanguage == null) {
+        var deviceLanguage = "EN";
+    }
+
+    console.log("sourceLang = " + sourceLang);
+    console.log("deviceLang = " + deviceLanguage);
+
+    if (deviceLanguage !== sourceLang) {
+        console.log("Attempting to translate \'" + txtSource + "\'");
+        var url = "https://translation.googleapis.com/language/translate/v2?key=" + apiKey;
+        url += "&source=" + sourceLang;
+        url += "&target=" + deviceLanguage;
+        url += "&q=" + escape(txtSource);
+        console.log("url = " + url);
+        $.get(url, function (data, status) {
+            console.log("returned: " + data.data.translations[0].translatedText);
+            return data.data.translations[0].translatedText;
+        });
+    } else {
+        console.log("Source and Target language are the same");
+        console.log("returned: " + txtSource);
+        return txtSource;
+    }
+}
