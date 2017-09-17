c50db3629d3245fe8eeef7b1d01abd405aeafa5f
akjoe <31557842+akjoe@users.noreply.github.com>
2017-09-16 19:02:23 -0700

    Convo pulling works

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -3,6 +3,7 @@
 // To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints,
 // and then run "window.location.reload()" in the JavaScript Console.
 (function () {
+
     "use strict";
 
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
